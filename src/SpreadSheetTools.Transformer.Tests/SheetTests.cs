2ab78f7db3a918a34bc6888f4e97ba0d356b6cd7
Tom Elmore <tom.elmore@easyroommate.com>
2016-05-03 13:35:34 +0100

    first pass at a reactive component.. didnt go well

---
+++ src/SpreadSheetTools.Transformer.Tests/SheetTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SpreadSheetTools.Transformer.Tests
+{
+    [TestFixture]
+    public class SheetTests
+    {
+        [Test]
+        public void CanDefineRange()
+        {
+            var data = new Dictionary<string, int>
+            {
+                { "A1", 0 },
+                { "A5", 1 },
+                { "A6", 2 },
+                { "A7", 3 },
+                { "A8", 4 },
+                { "A9", 5 },
+                { "A10", 6 },
+            };
+
+            var sheet = new Sheet();
+
+            sheet.Define("A1", "AVERAGE(A5:A10)");
+
+            sheet.Load(data);
+
+            int val = sheet.Get("A1");
+
+            Assert.AreEqual(3, val);
+        }
+
+
+    }
+}
---
+++ src/SpreadSheetTools.Transformer/Sheet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SpreadSheetTools.Transformer
+{
+    public class Sheet
+    {
+        private Dictionary<string, int> _data;
+        private AtlasGridCalculation _calc;
+
+        public void Define(string dest, string calcStr)
+        {
+            Tuple<int, int> index = IndexParser.Parse(dest);
+
+            var re = new Regex(@"AVERAGE\(([a-z]+\d+):([a-z]+\d+)\)", RegexOptions.IgnoreCase);
+
+            var m = re.Match(calcStr);
+
+            string start = m.Groups[1].Value;
+            string end = m.Groups[2].Value;
+
+            Tuple<int, int> indexStart = IndexParser.Parse(start);
+            Tuple<int, int> indexEnd = IndexParser.Parse(end);
+
+            AtlasGridCalculation calc = AtlasGridCalculation.Value(start);
+
+            for (var i = indexStart.Item2+1; i < indexEnd.Item2; i++)
+            {
+                string grid = IndexParser.Generate(indexStart.Item1, i);
+
+                calc = calc.Sum(grid);
+            }
+
+            _calc = calc;
+        }
+
+        public void Load(Dictionary<string, int> data)
+        {
+            _data = data;
+        }
+
+        public int Get(string coord)
+        {
+            return _calc.Eval(_data);
+        }
+    }
+}
